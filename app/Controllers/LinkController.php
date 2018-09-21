856133e1acea361e069a4e35ce8fc26466bb1355
lhie1 <lhie1x@gmail.com>
2018-09-21 14:07:54 +0800

    Update

--- app/Controllers/LinkController.php
+++ app/Controllers/LinkController.php
@@ -434,7 +434,7 @@ class LinkController extends BaseController
         foreach($items as $item) {
             if (substr($item['remark'],-3,3) != "SSR") {
                 if ($item['remark'] == "HK_IPLC_SS") {
-                    $item['remark'] = "HK_IPLC"
+                    $item['remark'] = "HK_IPLC";
                     if (URL::getSurgeObfs($item) != "") {
                         $proxy_group .= $item['remark'].' = custom,'.$item['address'].','.$item['port'].','.$item['method'].','.$item['passwd'].',https://dlercloud.com/SSEncrypt.module,'.URL::getSurgeObfs($item).',udp-relay=true,tfo=true'."\n";
                     }else{
