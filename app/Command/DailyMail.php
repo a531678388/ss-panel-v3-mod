ae05b4d84ddee8b4b8e996191676a87253e63fb2
lhie1 <lhie1x@gmail.com>
2018-09-25 14:28:53 +0800

    Update

--- app/Command/DailyMail.php
+++ app/Command/DailyMail.php
@@ -15,46 +15,48 @@ class DailyMail
 {
     public static function sendDailyMail()
     {
+        $sts = new Analytics();
+        $todayCheckinUser = $sts->getTodayCheckinUser();
+        
         $users = User::all();
-        $logs = Ann::orderBy('id', 'desc')->get();
-        $text1="";
+        if (Config::get('mailAnnouncementDays') == '0') {
+            $logs = Ann::orderBy('id', 'desc')->get();
+        } else {
+            $logs = Ann::where("date", ">", date("Y-m-d H:i:s", time()-86400*Config::get('mailAnnouncementDays')))->orderBy('id', 'desc')->get();
+        }
+        $ann="";
         
         foreach ($logs as $log) {
             if (strpos($log->content, "Links")===false) {
-                $text1=$text1.$log->content."<br><br>";
+                $ann.=$log->content."<br><br>";
             }
         }
-        
         $lastday_total = 0;
         
         foreach ($users as $user) {
-            $lastday = (($user->u+$user->d)-$user->last_day_t)/1024/1024;
             $lastday_total += (($user->u+$user->d)-$user->last_day_t);
             
             if ($user->sendDailyMail==1) {
                 echo "Send daily mail to user: ".$user->id;
-                $subject = Config::get('appName')." - 流量报告及公告";
+                $subject = Config::get('appName')." - 流量报告以及公告";
                 $to = $user->email;
-                $text = "<br><br>公告:<br><br>".$text1."<br><br>晚安！";
+                $text = "公告:<br><br>".$ann."<br><br>晚安！";
                 
                 
                 try {
                     Mail::send($to, $subject, 'news/daily-traffic-report.tpl', [
-                        "user" => $user,"text" => $text,"lastday"=>$lastday
+                        "user" => $user,"text" => $text
                     ], [
-                    ]);
+                    ], true);
                 } catch (Exception $e) {
                     echo $e->getMessage();
                 }
-                $text="";
             }
         }
-
-        $sts = new Analytics();
         
-        Telegram::Send("各位老爷少奶奶，我来为大家报告一下系统今天的运行状况哈~".
+        Telegram::Send("各位老爷少奶奶，为大家报告一下系统今天的运行状况~".
         PHP_EOL.
-        "今日签到人数:".$sts->getTodayCheckinUser().PHP_EOL.
+        "今日签到人数:".$todayCheckinUser.PHP_EOL.
         "今日使用总流量:".Tools::flowAutoShow($lastday_total).PHP_EOL.
         "晚安~"
         );
