3b1b23f9ac7f34deabc6afa0445e54b6e047ca78
lhie1 <lhie1x@gmail.com>
2018-09-30 14:18:03 +0800

    Update

--- app/Command/DailyMail.php
+++ app/Command/DailyMail.php
@@ -26,26 +26,27 @@ class DailyMail
         }
         
         $lastday_total = 0;
-        
+
         foreach ($users as $user) {
-            $lastday = (($user->u + $user->d) - $user->last_day_t) / 1024 / 1024;
             $lastday_total += (($user->u + $user->d) - $user->last_day_t);
-            
+        }
+
         $sts = new Analytics();
-        
-            Telegram::Send("各位老爷少奶奶，我来为大家报告一下系统今天的运行状况哈~".
+        Telegram::Send("各位老爷少奶奶，我来为大家报告一下系统今天的运行状况哈~".
             PHP_EOL.
             "今日签到人数:".$sts->getTodayCheckinUser().PHP_EOL.
             "今日使用总流量:".Tools::flowAutoShow($lastday_total).PHP_EOL.
             "晚安~"
             );
+        
+        foreach ($users as $user) {
+            $lastday = (($user->u + $user->d) - $user->last_day_t) / 1024 / 1024;
 
             if ($user->sendDailyMail == 1) {
                 echo "Send daily mail to user: ".$user->id;
                 $subject = Config::get('appName')." - 流量报告以及公告";
                 $to = $user->email;
                 $text = "公告:<br><br>".$text1."<br><br>晚安！";
-                
                 
                 try {
                     Mail::send($to, $subject, 'news/daily-traffic-report.tpl', [
