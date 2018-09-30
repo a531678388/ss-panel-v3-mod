ddcadcbf5c844e8d94cdee41af1c5cfb9c07cdc9
lhie1 <lhie1x@gmail.com>
2018-09-30 23:47:55 +0800

    Update

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -986,7 +986,7 @@ class UserController extends BaseController
 
         $res['ret'] = 1;
         $res['name'] = $shop->name;
-        if ($user->class != $shop->user_class() && $shop->traffic_package==0) {
+        if ($user->class != $shop->user_class() && !isset($shop->traffic_package()) {
             $res['name'] .= "<br>请注意：您正在购买不同等级的套餐，有效期将会重新计算<br>";
         }
         $res['credit'] = $coupon->credit." %";
